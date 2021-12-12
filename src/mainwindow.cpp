12bc09ca72298bdab853c927159bfc8a686208fd
Felipe Mattes <mattesfelipe@protonmail.com>
2021-12-12 11:38:33 -0300

    realizado a inserção na lista, cada arquivo selecionado irá adicionar uma linha.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -14,17 +14,24 @@ MainWindow::MainWindow ()
   set_slots();
 
   add_column("Id", m_Columns.m_col_id);
-  add_column("Nome", m_Columns.m_col_name);
+  add_column("Arquivo", m_Columns.m_col_name);
 
   add_column_w_mask ("Formatado", m_Columns.m_col_value,"%010f");
 
-  add_item (0,"teste", 231.52, 2);
+  //insere a barra de progresso para o campo percent
+  auto cell = Gtk::make_managed<Gtk::CellRendererProgress>();
+  int cols_count = m_TreeView.append_column("Restante", *cell);
+  auto pColumn = m_TreeView.get_column(cols_count - 1);
+  if(pColumn)
+    {
+      pColumn->add_attribute(cell->property_value(), m_Columns.m_col_percentage);
+    }
 
 }
 void MainWindow::set_layout ()
 {
-  set_title("Gtk::TreeView (ListStore) example");
-  set_default_size(400, 200);
+  set_title("Cryptographer");
+  set_default_size(1280, 720);
 
   m_VBox.set_margin(5);
   set_child(m_VBox);
@@ -39,23 +46,33 @@ void MainWindow::set_layout ()
   m_VBox.append(m_ScrolledWindow);
 
   m_refTreeModel = Gtk::ListStore::create(m_Columns);
+
   m_TreeView.set_model(m_refTreeModel);
 
+
+  m_Button_Add.set_label ("Adicionar");
+  m_Button_Compress.set_label ("Comprimir");
+  m_Button_Quit.set_label ("Fechar");
+  m_ButtonBox.set_spacing (33);
+  m_ButtonBox.append (m_Button_Add);
+  m_ButtonBox.append (m_Button_Compress);
   m_ButtonBox.append(m_Button_Quit);
   m_ButtonBox.set_margin(5);
   m_Button_Quit.set_hexpand(true);
+
   m_Button_Quit.set_halign(Gtk::Align::END);
+  m_Button_Compress.set_halign (Gtk::Align::END);
+  m_Button_Add.set_halign (Gtk::Align::END);
+
+  m_VBox.append (m_ButtonBox);
 
 }
 
 void MainWindow::set_slots ()
 {
   m_Button_Quit.signal_clicked().connect( sigc::mem_fun(*this,&MainWindow::on_click_m_Button_Quit));
-}
-
-void MainWindow::on_click_m_Button_Quit ()
-{
-  this->hide();
+  m_Button_Add.signal_clicked().connect (sigc::mem_fun (*this, &MainWindow::on_click_m_Button_Add));
+  m_Button_Compress.signal_clicked().connect (sigc::mem_fun (*this, &MainWindow::on_click_m_Button_Compress));
 }
 
 void MainWindow::add_item (unsigned int id, const Glib::ustring& name, double value, int percent)
@@ -76,4 +93,67 @@ template<typename Type>
 void MainWindow::add_column_w_mask (const Glib::ustring &title, const Gtk::TreeModelColumn<Type> &m_column, const Glib::ustring &mask)
 {
   m_TreeView.append_column_numeric (title, m_column, mask);
+}
+void MainWindow::on_click_m_Button_Add ()
+{
+  auto dialog = new Gtk::FileChooserDialog("Selecione um arquivo",
+                                           Gtk::FileChooser::Action::OPEN);
+  dialog->set_transient_for(*this);
+  dialog->set_modal(true);
+  dialog->signal_response().connect(sigc::bind(sigc::mem_fun(*this, &MainWindow::on_file_dialog_response), dialog));
+
+  //Adicionar botoes
+  dialog->add_button("_Abrir", Gtk::ResponseType::OK);
+  dialog->add_button("_Cancelar", Gtk::ResponseType::CANCEL);
+
+  //Adiciona os filtros de arquivos
+  auto filter_any = Gtk::FileFilter::create();
+  filter_any->set_name(" All ");
+  filter_any->add_pattern("*");
+  dialog->add_filter(filter_any);
+
+  //Mostra o dialogo e aguarda a response
+  dialog->show();
+}
+
+void MainWindow::on_click_m_Button_Quit ()
+{
+  this->hide();
+}
+void MainWindow::on_file_dialog_response (int response_id, Gtk::FileChooserDialog *dialog)
+{
+  switch (response_id)
+    {
+      case Gtk::ResponseType::OK:
+        {
+          std::cout << "Clicou em Abrir." << std::endl;
+
+          //retorna uma std:string
+          auto filename = dialog->get_file()->get_path();
+          std::cout << "Arquivo Selecionado: " <<  filename << std::endl;
+          add_file_list(filename);
+          break;
+        }
+      case Gtk::ResponseType::CANCEL:
+        {
+          std::cout << "Clicou em cancelar." << std::endl;
+          break;
+        }
+      default:
+        {
+          std::cout << "Outro Botao." << std::endl;
+          break;
+        }
+    }
+  delete dialog;
+}
+void MainWindow::on_click_m_Button_Compress ()
+{
+
+}
+void MainWindow::add_file_list (std::string &filename)
+{
+  file_count++;
+
+  add_item (file_count,filename, 0.00, 0);
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -7,6 +7,12 @@
 #include "gtkmm.h"
 #include "sigc++/sigc++.h"
 #include "listmodel.h"
+#include <utility>
+#include <iostream>
+#include "cryptopp/gzip.h"
+#include "cryptopp/files.h"
+
+
 class MainWindow : public Gtk::Window{
  public:
   MainWindow();
@@ -15,6 +21,10 @@ class MainWindow : public Gtk::Window{
  protected:
   //Signals
   void on_click_m_Button_Quit();
+  void on_click_m_Button_Add();
+  void on_click_m_Button_Compress();
+  //Signal de resposta do dialog
+  void on_file_dialog_response(int response_id, Gtk::FileChooserDialog *dialog);
 
   void set_layout();
   void set_slots();
@@ -29,9 +39,11 @@ class MainWindow : public Gtk::Window{
 
   Gtk::Box m_ButtonBox;
   Gtk::Button m_Button_Quit;
+  Gtk::Button m_Button_Add;
+  Gtk::Button m_Button_Compress;
 
   void add_item(unsigned int id, const Glib::ustring& name, double value, int percent);
-
+  void add_file_list(std::string& filename);
   //template para adicionar qualquer tipo para a treeview
   template <typename Type> inline
   void add_column(const Glib::ustring& title, const Gtk::TreeModelColumn<Type>& m_column);
@@ -41,6 +53,8 @@ class MainWindow : public Gtk::Window{
 
   //utilizar para adicionar na model
   Gtk::TreeRow m_Row;
+
+  short file_count = 0;
 };
 
 #endif //_MAINWINDOW_H_
